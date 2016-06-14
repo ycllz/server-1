d0781ea3cd8ad4d66a221918a616c82e03e34fd9
卢建波 <lujianbo@moqikaka.com>
2016-06-14 20:26:38 +0800

    commmit

--- src/main.cpp
+++ src/main.cpp
@@ -17,45 +17,9 @@
 using namespace std;
 
 int main(void) {
-
-	// testing short length
-	printf("testing short length\n");
-	short shortNum = 1;
-	int length = 1;
-	while(shortNum > 0) {
-		shortNum = shortNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
-
-	// testing int length
-	int intNum = 1;
-	length = 1;
-	printf("testing int length\n");
-	while(intNum > 0) {
-		intNum = intNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
-
-	// testing long length
-	long longNum = 1;
-	length = 1;
-	printf("testing long length\n");
-	while(longNum > 0) {
-		longNum = longNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
-
-	// testing long long length
-	long long longlongNum = 1;
-	length = 1;
-	printf("testing long long length\n");
-	while(longlongNum > 0) {
-		longlongNum = longlongNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
+	printf("short %d byte\n", sizeof(short));
+	printf("int %d byte\n", sizeof(int));
+	printf("long %d byte\n", sizeof(long));
+	printf("long long %d byte\n", sizeof(long long));
 	return 0;
 }
