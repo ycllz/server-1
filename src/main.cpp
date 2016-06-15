b39acc11f6da0c4e7b2e9cc499a8e4d38f2113fd
vimcoders <jimboo.lu@gmail.com>
2016-06-14 21:48:01 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -18,44 +18,11 @@ using namespace std;
 
 int main(void) {
 
-	// testing short length
-	printf("testing short length\n");
-	short shortNum = 1;
-	int length = 1;
-	while(shortNum > 0) {
-		shortNum = shortNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
+	// testing compile environment
+	printf("short length is %d\n", sizeof(short));
+	printf("int length is %d\n", sizeof(int));
+	printf("long length is %d\n", sizeof(long));
+	printf("long long is %d\n", sizeof(long long));
 
-	// testing int length
-	int intNum = 1;
-	length = 1;
-	printf("testing int length\n");
-	while(intNum > 0) {
-		intNum = intNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
-
-	// testing long length
-	long longNum = 1;
-	length = 1;
-	printf("testing long length\n");
-	while(longNum > 0) {
-		longNum = longNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
-
-	// testing long long length
-	long long longlongNum = 1;
-	length = 1;
-	printf("testing long long length\n");
-	while(longlongNum > 0) {
-		longlongNum = longlongNum << 1;
-		length++;
-	}
-	printf("length=%d\n", length);
 	return 0;
 }
